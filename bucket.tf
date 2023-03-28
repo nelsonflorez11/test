6e38d0ce84b2a909fd866d0f51cef5d3231f5610
Nelson Florez <nelsonflorez11@gmail.com>
2023-03-27 21:38:05 -0500

    iniciar

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
     module "bucket" {
         source = "git@github.com:nelsonflorez11/terraform-modules.git//aws/storage/s3-bucket?ref=feauture/module-s3"
  
-        bucket = "javier-estrada-tutorial-bucket"
+        bucket = "nelson-tutorial-bucket"
         bucket_objects = ["img", "doc", "www"]
     }
